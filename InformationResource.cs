cea7412d24afb65f9a2e1519f996c12b1011aefe
May_Be(Next_Time) <44321280+MadIntuition@users.noreply.github.com>
2020-11-02 18:29:11 +0300

    Add files via upload

---
+++ InformationResource.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ISAS.Inventory
+{
+    public class InformationResource
+    {
+        private string title;
+        private string  description;
+        private int count;
+        bool personaldata;
+        bool commercial_data;
+        bool statesecret;
+        int valueres;
+        int critlvl;
+        int category;
+        double threatsLvl;
+        double risk;
+        public string Title { get => title; set => title = value; }
+        public int Count { get => count; set => count = value; }
+        public string Description { get => description; set => description = value; }
+        public bool Personaldata { get => personaldata; set => personaldata = value; }
+        public bool Commercial_data { get => commercial_data; set => commercial_data = value; }
+        public bool Statesecret { get => statesecret; set => statesecret = value; }
+        public int Valueres { get => valueres; set => valueres = value; }
+        public int Critlvl { get => critlvl; set => critlvl = value; }
+        public int Category { get => category; set => category = value; }
+        public double ThreatsLvl { get => threatsLvl; set => threatsLvl = value; }
+        public double Risk { get => risk; set => risk = value; }
+
+        public InformationResource(string title, int count = 1, string desc = "", bool personal_data = false, bool commercial_data = false,
+            bool state_secret = false, int valueres = 0, int critlvl = 0, int category = 0, double threatsLvl = 0, double risk=0)
+        {
+            this.Title = title;
+            this.Count = count;
+            this.Description = desc;
+            this.Personaldata = personal_data;
+            this.Commercial_data = commercial_data;
+            this.Statesecret = state_secret;
+            this.Valueres = valueres;
+            this.Critlvl = critlvl;
+            this.Category = category;
+            this.ThreatsLvl = threatsLvl;
+            this.Risk = risk;
+        }
+
+        public void WriteToXml(string filename)
+        {
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+            XElement root = xdoc.Element("InformationResources");
+            // добавляем новый элемент
+            root.Add(new XElement("InformationResource",
+                        new XAttribute("title", this.Title),
+                        new XElement("count", this.Count),
+                        new XElement("description", this.Description),
+                        new XElement("Personaldata", this.Personaldata),
+                        new XElement("Commercial_data", this.Commercial_data),
+                        new XElement("Statesecret", this.Statesecret),
+                        new XElement("Valueres", this.Valueres),
+                        new XElement("Critlvl", this.Critlvl),
+                        new XElement("Category", this.Category)
+                        ));
+            xdoc.Save(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+        }
+        public static List<InformationResource> ReadFromXml(string filename)
+        {
+            List<InformationResource> result = new List<InformationResource>();
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\"+filename));
+            XElement root = xdoc.Element("InformationResources");
+            foreach (XElement resourceElement in root.Elements("InformationResource"))
+            {
+                string desc;
+                bool pd, cd, sts;
+                int val, crit, cat;
+                double risk, threatsLvl;
+                XAttribute titleAttribute = resourceElement.Attribute("title");
+                XElement countElement = resourceElement.Element("count");
+                try { desc = resourceElement.Element("description").Value;}
+                catch { desc = ""; }
+                try { pd = bool.Parse(resourceElement.Element("Personaldata").Value); }
+                catch { pd = false; }
+                try { cd = bool.Parse(resourceElement.Element("Commercial_data").Value); }
+                catch { cd = false; }
+                try { sts = bool.Parse(resourceElement.Element("Statesecret").Value); }
+                catch { sts = false; }
+                try { val = int.Parse(resourceElement.Element("Valueres").Value); }
+                catch { val = 0; }
+                try { crit = int.Parse(resourceElement.Element("Critlvl").Value); }
+                catch { crit = 0; }
+                try { cat = int.Parse(resourceElement.Element("Category").Value); }
+                catch { cat = 0; }
+                try { risk = double.Parse(resourceElement.Element("Risk").Value.Replace('.', ',')); }
+                catch { risk = 0; }
+                try { threatsLvl = double.Parse(resourceElement.Element("ThreatsLvl").Value.Replace('.', ',')); }
+                catch { threatsLvl = 0; }
+                result.Add(new InformationResource(titleAttribute.Value, int.Parse(countElement.Value),
+                    desc, pd, cd, sts, val, crit, cat, threatsLvl, risk)) ;
+            }
+            return result;
+        }
+
+        public void DeleteFromXml(string filename, string title)
+        {
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+            XElement root = xdoc.Element("InformationResources");
+            foreach (XElement resourceElement in root.Elements("InformationResource"))
+            {
+                if (resourceElement.Attribute("title").Value == title)
+                    resourceElement.Remove();
+            }
+            xdoc.Save(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+        }
+        public void EditXml(string filename)
+        {
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+            XElement root = xdoc.Element("InformationResources");
+            foreach (XElement resourceElement in root.Elements("InformationResource"))
+            {
+                if (resourceElement.Attribute("title").Value == this.Title)
+                {
+                    resourceElement.Element("count").SetValue( count.ToString());
+                    try { resourceElement.Element("description").SetValue(this.Description.ToString()); }
+                    catch { resourceElement.Add(new XElement("description", this.Description)); }
+                    try { resourceElement.Element("Personaldata").SetValue(this.Personaldata); }
+                    catch { resourceElement.Add(new XElement("Personaldata", this.Personaldata)); }
+                    try { resourceElement.Element("Commercial_data").SetValue(this.Commercial_data); }
+                    catch { resourceElement.Add(new XElement("Commercial_data", this.Commercial_data)); }
+                    try { resourceElement.Element("Statesecret").SetValue(this.Statesecret); }
+                    catch { resourceElement.Add(new XElement("Statesecret", this.Statesecret)); }
+                    try { resourceElement.Element("Valueres").SetValue(this.Valueres); }
+                    catch { resourceElement.Add(new XElement("Valueres", this.Valueres)); }
+                    try { resourceElement.Element("Critlvl").SetValue(this.Critlvl); }
+                    catch { resourceElement.Add(new XElement("Critlvl", this.Critlvl)); }
+                    try { resourceElement.Element("Category").SetValue(this.Category); }
+                    catch { resourceElement.Add(new XElement("Category", this.Category)); }
+                    try { resourceElement.Element("ThreatsLvl").SetValue(this.ThreatsLvl); }
+                    catch { resourceElement.Add(new XElement("ThreatsLvl", this.ThreatsLvl)); }
+                    try { resourceElement.Element("Risk").SetValue(this.Risk); }
+                    catch { resourceElement.Add(new XElement("Risk", this.Risk)); }
+                }
+            }
+            xdoc.Save(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+        }
+
+        public static InformationResource FindResourceXml(string filename, string title)
+        {
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+            XElement root = xdoc.Element("InformationResources");
+            foreach (XElement resourceElement in root.Elements("InformationResource"))
+            {
+                if (resourceElement.Attribute("title").Value == title)
+                {
+                    int count = int.Parse(resourceElement.Element("count").Value);
+                    string desc;
+                    bool pd, cd, sts;
+                    int val, crit, cat;
+                    double risk, threatsLvl;
+                    try { desc = resourceElement.Element("description").Value; }
+                    catch { desc = ""; }
+                    try { pd = bool.Parse(resourceElement.Element("Personaldata").Value); }
+                    catch { pd = false; }
+                    try { cd = bool.Parse(resourceElement.Element("Commercial_data").Value); }
+                    catch { cd = false; }
+                    try { sts = bool.Parse(resourceElement.Element("Statesecret").Value); }
+                    catch { sts = false; }
+                    try { val = int.Parse(resourceElement.Element("Valueres").Value); }
+                    catch { val = 0; }
+                    try { crit = int.Parse(resourceElement.Element("Critlvl").Value); }
+                    catch { crit = 0; }
+                    try { cat = int.Parse(resourceElement.Element("Category").Value); }
+                    catch { cat = 0; }
+                    try { risk = double.Parse(resourceElement.Element("Risk").Value.Replace('.', ',')); }
+                    catch { risk = 0; }
+                    try { threatsLvl = double.Parse(resourceElement.Element("ThreatsLvl").Value.Replace('.', ',')); }
+                    catch { threatsLvl = 0; }
+                    return new InformationResource(title, count, desc, pd, cd, sts, val,crit, cat, threatsLvl,risk);
+                }
+            }
+            return null;
+        }
+
+
+        public static void DeleteAllFromXml(string filename)
+        {
+            XDocument xdoc = XDocument.Load(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+            XElement root = xdoc.Element("InformationResources");
+            root.RemoveAll();
+            xdoc.Save(Environment.CurrentDirectory.Replace("bin\\Debug", "Inventory\\lists\\" + filename));
+        }
+    }
+}
---
+++ InformationResources.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ISAS.Inventory
+{
+    /// <summary>
+    /// Логика взаимодействия для InformationResources.xaml
+    /// </summary>
+    public partial class InformationResources : Window
+    {
+        List<InformationResource> current_resources = InformationResource.ReadFromXml("CurrentIR.xml");
+        public InformationResources()
+        {
+            InitializeComponent();
+            //MessageBox.Show(inf_resources[0].Title);
+            this.InfResList.ItemsSource = InformationResource.ReadFromXml("InformationResources.xml");
+            this.InfResCur.ItemsSource = InformationResource.ReadFromXml("CurrentIR.xml");
+        }
+
+        private void AddResouceClick(object sender, RoutedEventArgs e)
+        {
+            InformationResourcesAddNew addingWindow = new InformationResourcesAddNew();
+
+            if (addingWindow.ShowDialog() == true)
+            {
+                this.InfResList.ItemsSource = InformationResource.ReadFromXml("InformationResources.xml");
+                MessageBox.Show("Информационный ресурс \""+ addingWindow.ResourceTitle+"\" успешно добавлен");        
+            }
+
+        }
+
+        private void ToCurrentTableClick(object sender, RoutedEventArgs e)
+        {
+            List<InformationResource> selected_resources = InfResList.SelectedItems.Cast<InformationResource>().ToList();
+            List<InformationResource> helpList=new List<InformationResource>();
+            helpList.AddRange(current_resources);
+            //MessageBox.Show(selected_resources[0].Title);
+            foreach (InformationResource res in selected_resources)
+            {
+                helpList.Add(res);
+            }
+            this.InfResCur.ItemsSource = helpList;
+            current_resources.Clear();
+            current_resources.AddRange(helpList);
+        }
+
+        private void SaveCurResClick(object sender, RoutedEventArgs e)
+        {
+            InformationResource.DeleteAllFromXml("CurrentIR.xml");
+            foreach (InformationResource res in current_resources)
+            {
+                res.WriteToXml("CurrentIR.xml");          
+            }
+            MessageBox.Show("Изменения сохранены");
+        }
+
+        private void DeleteFromCurResClick(object sender, RoutedEventArgs e)
+        {
+            List<InformationResource> selected_resources = InfResCur.SelectedItems.Cast<InformationResource>().ToList();
+            List<InformationResource> helpList = new List<InformationResource>();
+            helpList.AddRange(current_resources);
+            foreach (InformationResource res in selected_resources)
+            {
+                helpList.Remove(res);
+            }
+            this.InfResCur.ItemsSource = helpList;
+            current_resources.Clear();
+            current_resources.AddRange(helpList);
+        }
+
+        private void DeleteResouceClick(object sender, RoutedEventArgs e)
+        {
+            List<InformationResource> selected_resources = InfResList.SelectedItems.Cast<InformationResource>().ToList();
+            List<InformationResource> helpList = new List<InformationResource>();
+            helpList.AddRange(current_resources);
+            foreach (InformationResource res in selected_resources)
+            {
+                helpList.Remove(res);
+                res.DeleteFromXml("InformationResources.xml", res.Title);
+            }
+            this.InfResList.ItemsSource =InformationResource.ReadFromXml("InformationResources.xml") ;
+        }
+
+        private void EditClick(object sender, RoutedEventArgs e)
+        {
+            if (!(InfResCur.SelectedItem is null))
+            {
+                InformationResource selected_resource = (InformationResource)InfResCur.SelectedItem;
+                //selected_resource = InformationResource.FindResourceXml("CurrentIR.xml", selected_resource.Title);
+                Editing editWindow = new Editing(selected_resource);
+                if (editWindow.ShowDialog() == true)
+                {
+                    selected_resource.Count = editWindow.Count;
+                    selected_resource.Description = editWindow.Description;
+                    selected_resource.Personaldata = editWindow.Personaldata;
+                    selected_resource.Commercial_data = editWindow.Commercial_data;
+                    selected_resource.Statesecret = editWindow.Statesecret;
+                    selected_resource.EditXml("CurrentIR.xml");
+                }
+                this.InfResCur.ItemsSource = InformationResource.ReadFromXml("CurrentIR.xml");
+            }
+        }
+    }
+}
